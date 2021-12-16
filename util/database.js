f8ce3e62ae52d763ed989417c6072b7d8f436cb0
Rayane Cristina <68652767+raydevcp20@users.noreply.github.com>
2021-12-16 07:52:32 -0300

    alterando database

--- util/database.js
+++ util/database.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  database: 'espalhandobem',
-  password: '',
+  host: 'us-cdbr-east-05.cleardb.net',
+  user: 'b8dc133e0f516c',
+  database: 'heroku_eb86933fec20644',
+  password: '11f4917b',
 });
 
 module.exports = pool.promise();
