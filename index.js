fdbf5a709efb382a190a644a2525c6dd65656544
Rayane Cristina <68652767+raydevcp20@users.noreply.github.com>
2021-12-16 09:44:50 -0300

    implementação do cors

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const authRoutes = require('./routes/auth');
 const postsRoutes = require('./routes/posts');
 const photoRoutes = require('./routes/photo');
-// const cors = require('cors');
+const cors = require('cors');
 
 // parse application/json
 app.use(express.json());
@@ -15,12 +15,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 //get permisions and disable CORS
-// app.use(cors());
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods','GET, POST, PUT, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Accept, X-Custom-Header, Authorization');
-
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods','GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Accept, X-Custom-Header, Authorization');
+  app.use(cors());
     if (req.method === 'OPTIONS') {
       return res.status(200).end();
     }
